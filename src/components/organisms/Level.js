11db640afcb6b95256142e912b4c16b699f64bb8
lagora <lagora.franck@gmail.com>
2017-11-25 23:14:11 +0100

    wip Level cleaning

--- src/components/organisms/Level.js
+++ src/components/organisms/Level.js
@@ -1,29 +1,28 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
 import { Ground } from '../atoms/Ground';
 import { Light } from '../atoms/Light';
 import { LevelArea } from './LevelArea';
 import { Player } from './Player';
 import { makeAreas } from '../../utils';
 
-export const Level = ({ level }) => (
+export const Level = ({ level, player }) => (
   <a-scene
     shadow="type: pcfsoft"
     fog={`type: exponential; color: #${level.lightMode === 'night' ? '000' : 'ccccff'}; density: 0.0015`}
     physics="debug: true"
-    effects="#mix(0.01 0. 0. 0.)"
   >
     <Light mode={level.lightMode} />
     <Ground />
     <a-sky color={level.lightMode === 'night' ? '#000' : '#fff'} />
-    <Player
-      position={{ x: (level.size * 5), y: level.size * 0, z: (level.size * 5)}}
-      rotation={{ x: 0, y: 0, z: 0 }}
-    />
+    <Player {...player} />
     <a-box position="40 1.5 37.5" shadow="receive: true;" />
     {makeAreas(LevelArea)(level.areas)}
   </a-scene>
 );
 
-Level.propTypes = {};
+Level.propTypes = {
+  level: PropTypes.shape({}).isRequired,
+};
 
 export default Level;
