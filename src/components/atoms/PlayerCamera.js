5b052e8faaed88946587fcb0f50bfd7dc3d16e1c
lagora <lagora.franck@gmail.com>
2017-11-26 01:18:29 +0100

    extract camera from player to PlayerCamera

---
+++ src/components/atoms/PlayerCamera.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { position, rotation } from '../proptypes';
+import { xyzToString } from '../../utils';
+
+export const Player = props => (
+  <a-entity
+    id="camera"
+    camera="fov: 90; zoom: 1;"
+    position={
+      xyzToString({
+        ...props.self.position,
+        y: props.self.position.y + 3,
+        z: props.self.position.z + 4,
+      })
+    }
+    orbit-controls={[
+      // ['enabled', props.camera.player.thirdPerson],
+      ['autoRotate', false],
+      ['target', '#player'],
+      ['enableDamping', true],
+      ['enableZoom', false],
+      ['rotateSpeed', false],
+      ['dampingFactor', 0.125],
+      ['rotateSpeed', 0.25],
+      ['minDistance', 3],
+      ['maxDistance', 100],
+      ['enableKeys', false],
+      ['minPolarAngle', 0.5],
+      ['maxPolarAngle', Math.PI / 2.1],
+      ['rotateTo', {
+        ...props.self.position,
+        y: props.self.position.y + 2,
+      }],
+    ].map(markup => markup.join(':')).join(';')}
+    mouse-cursor=""
+    look-controls-enabled="false"
+  />
+);
+
+PlayerCamera.propTypes = {
+  self: PropTypes.shape({
+    position,
+    rotation,
+  }).isRequired,
+};
+
+export default PlayerCamera;
--- src/components/organisms/Player.js
+++ src/components/organisms/Player.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 // import { Camera } from '../atoms/Camera';
 import { camera, position, rotation } from '../proptypes';
+import { PlayerCamera } from '../atoms/PlayerCamera';
 import { xyzToString } from '../../utils';
 
 export const Player = props => (
@@ -21,38 +22,7 @@ export const Player = props => (
       depth="1"
       material="color: #F90"
     />
-    <a-entity
-      id="camera"
-      camera="fov: 90; zoom: 1;"
-      position={
-        xyzToString({
-          ...props.self.position,
-          y: props.self.position.y + 3,
-          z: props.self.position.z + 4,
-        })
-      }
-      orbit-controls={[
-        // ['enabled', props.camera.player.thirdPerson],
-        ['autoRotate', false],
-        ['target', '#player'],
-        ['enableDamping', true],
-        ['enableZoom', false],
-        ['rotateSpeed', false],
-        ['dampingFactor', 0.125],
-        ['rotateSpeed', 0.25],
-        ['minDistance', 3],
-        ['maxDistance', 100],
-        ['enableKeys', false],
-        ['minPolarAngle', 0.5],
-        ['maxPolarAngle', Math.PI / 2.1],
-        ['rotateTo', {
-          ...props.self.position,
-          y: props.self.position.y + 2,
-        }],
-      ].map(markup => markup.join(':')).join(';')}
-      mouse-cursor=""
-      look-controls-enabled="false"
-    />
+    <PlayerCamera {...props} />
     {/* <Camera
       position={{ x: 0, y: 0, z: 1.5 }}
       rotation={{ x: 9, y: -30, z: 0 }}
